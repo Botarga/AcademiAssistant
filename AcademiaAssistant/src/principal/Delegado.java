8dafcc2a6d16a6bcb4dd2abfe48e606fb3a60eac
Mario <mariovivasdavid@hotmail.es>
2015-06-24 19:33:02 +0200

    Agregada la clase Delegado
    Se ha agregado la clase delegado

---
+++ AcademiaAssistant/src/principal/Delegado.java
@@ -0,0 +1,13 @@
+package principal;
+
+/**
+ *
+ * @author botarga
+ */
+public class Delegado extends Alumno{
+    
+    /*-----CONSTRUCTORES-----*/
+    public Delegado(String nombre, String login, String contrasenya, String dni){
+        super(nombre, login, contrasenya, dni);
+    }
+}
